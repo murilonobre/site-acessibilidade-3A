e87a7c690030465876e284f52955b23105321352
murilonobre <126820432+murilonobre@users.noreply.github.com>
2025-04-09 10:50:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 
+document. addEventListenner('DONContentLoaded', function(){
+  const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+  const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibiidade')
+
+  botaoDeAcessibilidade.addEventListenner('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessbilidade.classList.toggle('apresenta-lista')
+
+    cont botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') ===
+      botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoselecionado)
+
+  })
+
+  const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+  const diminuiFonteBoto = document.getElementById('diminuir-fonte');
+  const alternaContraste = document.getElementById('alterna-contraste')
+  let tamanhoAtualFonte = 1;
+  aumentaFonteBotao.addEventListenner('click', function(){scrollRevael().reveal
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = ${tamanhoAtualFonte}rem
+    
+  })
+
+  diminuiFonteBotao.addEventListenner('click', function(){
+    document.body.style.fontSize = ${tamanhoAtualFonte}rem
+
+  })
+
+  alternaConstraste.addEventListenner('click', function(){
+    document.body.classList.toggle('alto-contraste')
+  })
+})
+
+scrollRevael().reveal('#inicio',{delay: 500});
+scrollRevael().reveal('#tropicalia', {delay: 500});
+scrollRevael().reveal('#galeria', {delay: 500});
+scrollRevael().reveal('#contato', {delay: 500});
